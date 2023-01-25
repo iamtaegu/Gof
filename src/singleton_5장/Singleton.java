03ebb982d353f875ce783e259aaec9c5cbab8f61
iamtaegu <gmdals45@gmail.com>
2023-01-25 11:44:15 +0900

    [2023/01/25 2,3,4,5장 review & 주석 추가]

--- src/singleton_5장/Singleton.java
+++ src/singleton_5장/Singleton.java
@@ -2,8 +2,13 @@ package singleton_5장;
 
 public class Singleton {
 
+    // 클래스 필드에 인스턴스를 생성
+    // 클래스가 최초 호출되는 시점에 인스턴스 생성
     private static Singleton singleton = new Singleton();
 
+    /***
+     * 생성자가 private이기 때문에 외부에서 인스턴스를 생성할 수 없음
+     */
     private Singleton() {
         System.out.println("인스턴스를 생성했습니다.");
     }
